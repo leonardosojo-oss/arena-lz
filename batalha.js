a2b4b2b75ec44aea78c1189699b4fffa88c05ed4
leonardosojo-oss <leonardo.sojo@escola.pr.gov.br>
2026-05-27 11:06:19 -0300

    Add files via upload

--- batalha.js
+++ batalha.js
@@ -6,15 +6,19 @@ function lutar(){
     if(!arma){
         log.innerHTML= "você esqueceu de equipar uma arma"
     }
-    let dado = Math.floor(Math.random() * 20) + 1
-    
+    let dado = Math.floor(Math.random() * 20) + 1;
+
+    let resultado = `Dado: ${dado} | <strong>${classe}</strong> com <strong>${arma}</strong>`;
+    switch(classe){
+    case "guerreiro":
+        if(arma.toLowerCase()=== "espada"&& dado >5){
+            resultado += "<span class= 'sucesso'>SUCESSO: VOCÊ você decapitou o monstro com um golpe!</span"
+
+        }
+        
+    }
 
 
+}let dado = Math.floor(Math.random() * 20) + 1;
 
-
-
-
-
-
-
-}
+let resultado = `Dado: ${dado} | <strong>${classe}</strong> com <strong>${arma}</strong>`;
